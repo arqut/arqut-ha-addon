dc40bafd3774f5dca4458a02d604d3320da0d12c
Tuan Phan <tphanx@gmail.com>
2025-09-23 21:31:30 +0300

    fix: typo

--- arqut-edge/run.sh
+++ arqut-edge/run.sh
@@ -11,6 +11,6 @@ if [ ! -d $DB_DIR ]; then
   bashio::log.info "Create db dir $DB_DIR"
 fi
 
-bashio::log.info "Starting Aequt Edge with data_dir=$DATA_DIR, log_level=$LOG_LEVEL ..."
+bashio::log.info "Starting Arqut Edge with data_dir=$DATA_DIR, log_level=$LOG_LEVEL ..."
 
 SME_DATA_DIR="$DATA_DIR" /arqut-edge -haaddon -loglevel=$LOG_LEVEL
